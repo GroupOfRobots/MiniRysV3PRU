95de38ed9e8aa25ad641a63c5b7a1d995f654b01
kamfor <kfforex@gmail.com>
2016-11-12 11:51:57 +0100

    initial

---
+++ Debug/main.d
@@ -0,0 +1,35 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /opt/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.3/include/stdint.h
+main.obj: /opt/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.3/include/stdio.h
+main.obj: /opt/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.3/include/linkage.h
+main.obj: /opt/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.3/include/stdarg.h
+main.obj: /opt/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.3/include/stdlib.h
+main.obj: ../../../../../include/am335x/pru_cfg.h
+main.obj: ../../../../../include/am335x/pru_intc.h
+main.obj: ../../../../../include/rsc_types.h
+main.obj: ../../../../../include/pru_types.h
+main.obj: ../../../../../include/pru_rpmsg.h
+main.obj: ../../../../../include/pru_virtqueue.h
+main.obj: ../../../../../include/pru_virtio_ring.h
+main.obj: ../resource_table_1.h
+main.obj: /opt/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.3/include/stddef.h
+main.obj: ../../../../../include/pru_virtio_ids.h
+
+../main.c: 
+/opt/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.3/include/stdint.h: 
+/opt/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.3/include/stdio.h: 
+/opt/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.3/include/linkage.h: 
+/opt/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.3/include/stdarg.h: 
+/opt/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.3/include/stdlib.h: 
+../../../../../include/am335x/pru_cfg.h: 
+../../../../../include/am335x/pru_intc.h: 
+../../../../../include/rsc_types.h: 
+../../../../../include/pru_types.h: 
+../../../../../include/pru_rpmsg.h: 
+../../../../../include/pru_virtqueue.h: 
+../../../../../include/pru_virtio_ring.h: 
+../resource_table_1.h: 
+/opt/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.3/include/stddef.h: 
+../../../../../include/pru_virtio_ids.h: 
