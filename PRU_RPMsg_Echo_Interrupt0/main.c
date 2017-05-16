8dc92859b73860586a8a4ca9beec492a86551460
mjbogusz <mjbogusz+github@gmail.com>
2017-05-16 16:54:51 +0200

    pru0 (sonars): refactored to echo all sonars at once

--- PRU_RPMsg_Echo_Interrupt0/main.c
+++ PRU_RPMsg_Echo_Interrupt0/main.c
@@ -42,185 +42,184 @@
 #include <pru_rpmsg.h>
 #include "resource_table_0.h"
 
-volatile register uint32_t __R31;
-
-struct distance_frame{
+struct DistanceFrame {
 	uint16_t front;
 	uint16_t back;
 	uint16_t top;
 };
 
-/* Host-0 Interrupt sets bit 30 in register R31 */
-#define HOST_INT			((uint32_t) 1 << 30)
+#define TRIG_BIT 4
+#define ECHO1_BIT 3
+#define ECHO2_BIT 5
+#define ECHO3_BIT 2
 
-/* The PRU-ICSS system events used for RPMsg are defined in the Linux device tree
- * PRU0 uses system event 16 (To ARM) and 17 (From ARM)
- * PRU1 uses system event 18 (To ARM) and 19 (From ARM)
- */
-#define TO_ARM_HOST			16
-#define FROM_ARM_HOST			17
+// Host-0 Interrupt sets bit 30 in register R31
+#define HOST_INT ((uint32_t) 1 << 30)
 
-/*
- * Using the name 'rpmsg-pru' will probe the rpmsg_pru driver found
- * at linux-x.y.z/drivers/rpmsg/rpmsg_pru.c
- */
-#define CHAN_NAME			"rpmsg-pru"
-#define CHAN_DESC			"Channel 30"
-#define CHAN_PORT			30
+// The PRU-ICSS system events used for RPMsg are defined in the Linux device tree
+// PRU0 uses system event 16 (To ARM) and 17 (From ARM)
+#define TO_ARM_HOST 16
+#define FROM_ARM_HOST 17
 
+// Using the name 'rpmsg-pru' will probe the rpmsg_pru driver found at linux-x.y.z/drivers/rpmsg/rpmsg_pru.c
+#define CHAN_NAME "rpmsg-pru"
+#define CHAN_DESC "Channel 30"
+#define CHAN_PORT 30
 
-#define PRU_OCP_RATE_HZ		(200 * 1000 * 1000)
+#define PRU_OCP_RATE_HZ (200 * 1000 * 1000)
 
-#define TRIG_PULSE_US		10
+#define TRIG_PULSE_US 10
 
 #define GPIO0_BASE 0x44E07000
 #define GPIO1_BASE 0x4804C000
 #define GPIO2_BASE 0x481AC000
 #define GPIO3_BASE 0x481AE000
 
-#define GPIO2_OE		(*(volatile uint32_t *)(GPIO2_BASE + 0x134))
-#define GPIO2_DATAIN		(*(volatile uint32_t *)(GPIO2_BASE + 0x138))
-#define GPIO2_CLEARDATAOUT	(*(volatile uint32_t *)(GPIO2_BASE + 0x190))
-#define GPIO2_SETDATAOUT	(*(volatile uint32_t *)(GPIO2_BASE + 0x194))
+#define GPIO2_OE (*(volatile uint32_t *)(GPIO2_BASE + 0x134))
+#define GPIO2_DATAIN (*(volatile uint32_t *)(GPIO2_BASE + 0x138))
+#define GPIO2_CLEARDATAOUT (*(volatile uint32_t *)(GPIO2_BASE + 0x190))
+#define GPIO2_SETDATAOUT (*(volatile uint32_t *)(GPIO2_BASE + 0x194))
 
-#define TRIG_BIT		4 //to change used by wlink
-#define ECHO1_BIT		3 //to change used by wlink
-#define ECHO2_BIT		5 //to change used by wlink
-#define ECHO3_BIT		2 //to change used by wlink
-
-
-/*
- * Used to make sure the Linux drivers are ready for RPMsg communication
- * Found at linux-x.y.z/include/uapi/linux/virtio_config.h
- */
+// Used to make sure the Linux drivers are ready for RPMsg communication
+// Found at linux-x.y.z/include/uapi/linux/virtio_config.h
 #define VIRTIO_CONFIG_S_DRIVER_OK	4
 
+volatile register uint32_t __R31;
 uint8_t payload[RPMSG_BUF_SIZE];
 
-void hc_sr04_init(void)
-{
-	/* Enable OCP access */
+void sonarsInit() {
+	// Enable OCP access
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
-	/*
-	 * Don't bother with PRU GPIOs. Our timing requirements allow
-	 * us to use the "slow" system GPIOs.
-	 */
-	GPIO2_OE &= ~(1u << TRIG_BIT);	/* output */
-	GPIO2_OE |= (1u << ECHO1_BIT);	/* input */
-	GPIO2_OE |= (1u << ECHO2_BIT);	/* input */
-	GPIO2_OE |= (1u << ECHO3_BIT);	/* input */
+	// Don't bother with PRU GPIOs. Our timing requirements allow us to use the "slow" system GPIOs.
+	// Trigger: output
+	GPIO2_OE &= ~(1u << TRIG_BIT);
+	// Echos: input
+	GPIO2_OE |= (1u << ECHO1_BIT);
+	GPIO2_OE |= (1u << ECHO2_BIT);
+	GPIO2_OE |= (1u << ECHO3_BIT);
 }
 
-int hc_sr04_measure_pulse(int snumber) //snumber - number of sonar from 1 to 3
-{
-	bool echo, timeout;
-
-	/* pulse the trigger for 10us */
+// Measure sonars pulse time in microseconds
+void sonarsMeasurePulse(uint64_t & pulse1, uint64_t & pulse2, uint64_t & pulse3) {
+	// pulse the trigger for 10us
 	GPIO2_SETDATAOUT = 1u << TRIG_BIT;
 	__delay_cycles(TRIG_PULSE_US * (PRU_OCP_RATE_HZ / 1000000));
 	GPIO2_CLEARDATAOUT = 1u << TRIG_BIT;
 
-	/* Enable counter */
+	// Enable counter
 	PRU0_CTRL.CYCLE = 0;
 	PRU0_CTRL.CTRL_bit.CTR_EN = 1;
 
-	/* wait for ECHO to get high */
-	do {
-		if(snumber ==1)echo = !!(GPIO2_DATAIN & (1u << ECHO1_BIT));
-		if(snumber ==2)echo = !!(GPIO2_DATAIN & (1u << ECHO2_BIT));
-		if(snumber ==3)echo = !!(GPIO2_DATAIN & (1u << ECHO3_BIT));
-		timeout = PRU0_CTRL.CYCLE > PRU_OCP_RATE_HZ;
-	} while (!echo && !timeout);
+	uint64_t sonar1EchoStart = 0;
+	uint64_t sonar2EchoStart = 0;
+	uint64_t sonar3EchoStart = 0;
 
+	bool timeout = false;
+	// wait for ECHO to get high
+	while (!timeout && (!sonar1EchoStart || !sonar2EchoStart || !sonar3EchoStart)) {
+		if (!sonar1EchoStart && !(GPIO2_DATAIN & (1u << ECHO1_BIT))) {
+			sonar1EchoStart = PRU0_CTRL.CYCLE;
+		}
+		if (!sonar2EchoStart && !(GPIO2_DATAIN & (1u << ECHO2_BIT))) {
+			sonar2EchoStart = PRU0_CTRL.CYCLE;
+		}
+		if (!sonar3EchoStart && !(GPIO2_DATAIN & (1u << ECHO3_BIT))) {
+			sonar3EchoStart = PRU0_CTRL.CYCLE;
+		}
+
+		timeout = PRU0_CTRL.CYCLE > PRU_OCP_RATE_HZ;
+	}
+
+	if (timeout) {
+		pulse1 = -1;
+		pulse2 = -1;
+		pulse3 = -1;
+		return;
+	} else {
+		pulse1 = 0;
+		pulse2 = 0;
+		pulse3 = 0;
+	}
+
+	uint64_t timeoutStart = PRU0_CTRL.CYCLE;
+
+	// measure the "high" pulse length
+	while (!timeout && (!pulse1 || !pulse2 || !pulse3)) {
+		if (!pulse1 && GPIO2_DATAIN & (1u << ECHO1_BIT)) {
+			pulse1 = PRU0_CTRL.CYCLE - sonar1EchoStart;
+		}
+		if (!pulse2 && GPIO2_DATAIN & (1u << ECHO2_BIT)) {
+			pulse2 = PRU0_CTRL.CYCLE - sonar2EchoStart;
+		}
+		if (!pulse3 && GPIO2_DATAIN & (1u << ECHO3_BIT)) {
+			pulse3 = PRU0_CTRL.CYCLE - sonar3EchoStart;
+		}
+
+		timeout = (PRU0_CTRL.CYCLE - timeoutStart) > PRU_OCP_RATE_HZ;
+	}
+
+	// Disable counter
 	PRU0_CTRL.CTRL_bit.CTR_EN = 0;
 
-	if (timeout)
-		return -1;
+	if (timeout) {
+		pulse1 = -1;
+		pulse2 = -1;
+		pulse3 = -1;
+		return;
+	}
 
-	/* Restart the counter */
-	PRU0_CTRL.CYCLE = 0;
-	PRU0_CTRL.CTRL_bit.CTR_EN = 1;
-
-	/* measure the "high" pulse length */
-	do {
-		if(snumber ==1)echo = !!(GPIO2_DATAIN & (1u << ECHO1_BIT));
-		if(snumber ==2)echo = !!(GPIO2_DATAIN & (1u << ECHO2_BIT));
-		if(snumber ==3)echo = !!(GPIO2_DATAIN & (1u << ECHO3_BIT));
-		timeout = PRU0_CTRL.CYCLE > PRU_OCP_RATE_HZ;
-	} while (echo && !timeout);
-
-	PRU0_CTRL.CTRL_bit.CTR_EN = 0;
-
-	if (timeout)
-		return -1;
-
-	uint64_t cycles = PRU0_CTRL.CYCLE;
-
-	return cycles / ((uint64_t)PRU_OCP_RATE_HZ / 1000000);
+	pulse1 /= ((uint64_t)PRU_OCP_RATE_HZ / 1000000);
+	pulse2 /= ((uint64_t)PRU_OCP_RATE_HZ / 1000000);
+	pulse3 /= ((uint64_t)PRU_OCP_RATE_HZ / 1000000);
 }
 
+void sonarsMeasureDistance(uint16_t & distance1, uint16_t & distance2, uint16_t & distance3) {
+	uint64_t pulse1, pulse2, pulse3;
+	sonarsMeasurePulse(pulse1, pulse2, pulse3);
 
-uint16_t measure_distance_mm(int snumber)//snumber - sonar number from 1 to 3
-{
-	int t_us = hc_sr04_measure_pulse(snumber);
-	int d_mm;
-
-	/*
-	 * Print the distance received from the sonar
-	 * At 20 degrees in dry air the speed of sound is 3422 mm/sec
-	 * so it takes 2.912 us to make 1 mm, i.e. 5.844 us for a
-	 * roundtrip of 1 mm.
-	 */
-	d_mm = (t_us * 1000) / 5844;
-	if (t_us < 0)
-		d_mm = -1;
-
-	return d_mm;
+	distance1 = pulse1 * 1000 / 5844;
+	distance2 = pulse2 * 1000 / 5844;
+	distance3 = pulse3 * 1000 / 5844;
 }
 
-
-/*
- * main.c
- */
-void main(void)
-{
+void main() {
 	struct pru_rpmsg_transport transport;
 	uint16_t src, dst, len;
 	volatile uint8_t *status;
-	struct distance_frame* distances;
-	distances = (struct distance_frame*) malloc(sizeof(struct distance_frame));
+	struct DistanceFrame* distances;
+	distances = (struct DistanceFrame*) malloc(sizeof(struct DistanceFrame));
 
+	sonarsInit();
 
-	hc_sr04_init(); //sonars initialization
-	/* Allow OCP master port access by the PRU so the PRU can read external memories */
+	// Allow OCP master port access by the PRU so the PRU can read external memories
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
-
-	/* Clear the status of the PRU-ICSS system event that the ARM will use to 'kick' us */
+	// Clear the status of the PRU-ICSS system event that the ARM will use to 'kick' us
 	CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
 
-	/* Make sure the Linux drivers are ready for RPMsg communication */
+	// Make sure the Linux drivers are ready for RPMsg communication
 	status = &resourceTable.rpmsg_vdev.status;
 	while (!(*status & VIRTIO_CONFIG_S_DRIVER_OK));
 
-	/* Initialize the RPMsg transport structure */
+	// Initialize the RPMsg transport structure
 	pru_rpmsg_init(&transport, &resourceTable.rpmsg_vring0, &resourceTable.rpmsg_vring1, TO_ARM_HOST, FROM_ARM_HOST);
-	/* Create the RPMsg channel between the PRU and ARM user space using the transport structure. */
+	// Create the RPMsg channel between the PRU and ARM user space using the transport structure.
 	while (pru_rpmsg_channel(RPMSG_NS_CREATE, &transport, CHAN_NAME, CHAN_DESC, CHAN_PORT) != PRU_RPMSG_SUCCESS);
-	while (1) {
-		/* Check bit 30 of register R31 to see if the ARM has kicked us */
-		if (__R31 & HOST_INT) {
-			/* Clear the event status */
-			CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
-			/* Receive all available messages, multiple messages can be sent per kick */
-			while (pru_rpmsg_receive(&transport, &src, &dst, payload, &len) == PRU_RPMSG_SUCCESS) {
-				/* Echo the message back to the same address from which we just received */
-				distances->front=(uint16_t)measure_distance_mm(1);
-				distances->back=(uint16_t)measure_distance_mm(2);
-				distances->top=(uint16_t)measure_distance_mm(3);
 
-				pru_rpmsg_send(&transport, dst, src, distances, len);
-			}
+	while (1) {
+		// Check bit 30 of register R31 to see if the ARM has kicked us
+		if (!(__R31 & HOST_INT)) {
+			continue;
+		}
+
+		// Clear the event status
+		CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
+		// Receive all available messages - multiple messages can be sent per kick
+		while (pru_rpmsg_receive(&transport, &src, &dst, payload, &len) == PRU_RPMSG_SUCCESS) {
+			// Do the measurement
+			sonarsMeasureDistance(distances->front, distances->back, distances->top);
+			// Echo the message back to the same address from which we just received
+			pru_rpmsg_send(&transport, dst, src, distances, len);
 		}
 	}
 }
