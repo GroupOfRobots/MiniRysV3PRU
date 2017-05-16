13677ff3db9cc9fc5d5ee3ca08e328cfbf5e4da0
mjbogusz <mjbogusz+github@gmail.com>
2017-05-16 15:12:07 +0200

    cleanup: removed editor/build files

--- PRU_RPMsg_Echo_Interrupt1/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"/home/kamil/ti-processor-sdk/example-applications/pru-icss-5.0.1/lib/rpmsg_lib.lib"
-
